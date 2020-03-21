36b8cb3947b84adb33e1383acce4fbecf4ba6a0c
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2020-03-21 12:26:03 +0100

    UPSTREAM: lockdep: Annotate irq_work
    Mark irq_work items with IRQ_WORK_HARD_IRQ which should be invoked in
    hardirq context even on PREEMPT_RT. IRQ_WORK without this flag will be
    invoked in softirq context on PREEMPT_RT.
    
    Set ->irq_config to 1 for the IRQ_WORK items which are invoked in softirq
    context so lockdep knows that these can safely acquire a spinlock_t.
    
    Change-Id: I00138dc48d24a5bb4791cc89835fdce553c41277
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20200321113242.643576700@linutronix.de

--- kernel/irq_work.c
+++ kernel/irq_work.c
@@ -164,8 +164,11 @@ static void irq_work_run_list(struct llist_head *list)
 		atomic_xchg(&work->flags, flags);
 
 		check_start_time(ts);
+		lockdep_irq_work_enter(work);
 		work->func(work);
+		lockdep_irq_work_exit(work);
 		check_process_time("irq_work %ps", ts, work->func);
+
 		/*
 		 * Clear the BUSY bit and return to the free state if
 		 * no-one else claimed it meanwhile.
